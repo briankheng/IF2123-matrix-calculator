e017cb8c0807fd6d797f5f9e2cd3a9a19d351cc0
frankiehuangg <frankie.huangg@gmail.com>
2022-09-26 14:10:46 +0700

    initial commit

---
+++ src/Bicubic.java
@@ -0,0 +1,16 @@
+import lib.BalikanAdjoin;
+import lib.Cramer;
+import lib.Determinant;
+import lib.Kofaktor;
+import lib.Matrix;
+import lib.SPL;
+
+public class Bicubic
+{
+	public static double bicubic(Matrix matrix, double a, double b)
+	// Input matrix 4x4, berisi nilai f(i,j) dan nilai (a,b)
+	// Output nilai f(a,b)
+	{
+		// invers matrix untuk -1<=x<=2 dan -1<=y<=2
+	}
+}
