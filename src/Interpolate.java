08c6abfe96c9134b9a715a1ca8016d2f5d298c6b
BrianKheng <briankheng63@gmail.com>
2022-09-27 20:40:45 +0700

    fix: Change Matrix Inverse method to Gauss Jordan

--- src/Interpolate.java
+++ src/Interpolate.java
@@ -69,7 +69,9 @@ public class Interpolate {
                 }
             }
         }
-        double[] a = SPL.InversMatrix(M);
+        SPL solution = new SPL();
+        solution.GaussJordan(M);
+        double[] a = solution.x;
         
         // Input nilai X dan output f(X)
         System.out.printf("Masukkan nilai x yang akan ditaksir nilai fungsinya: ");
@@ -81,10 +83,24 @@ public class Interpolate {
         }
         System.out.printf("Luaran untuk interpolasi adalah f(x) = ");
         for(int i = n-1; i >= 0; i--){
-            if(i == 0) System.out.printf("%f,\n", a[i]);
-            else System.out.printf("%fx^%d + ", a[i], i);
+            if(i == n-1){
+                if(a[i] < 0) System.out.printf("-%fx^%d", Math.abs(a[i]), i);
+                else System.out.printf("%fx^%d", Math.abs(a[i]), i);
+            }
+            else if(i == 0){
+                if(a[i] < 0) System.out.printf(" - %f,\n", Math.abs(a[i]));
+                else System.out.printf(" + %f,\n", Math.abs(a[i]));
+            }
+            else if(i == 1){
+                if(a[i] < 0) System.out.printf(" - %fx", Math.abs(a[i]));
+                else System.out.printf(" + %fx", Math.abs(a[i]));
+            }
+            else{
+                if(a[i] < 0) System.out.printf(" - %fx^%d", Math.abs(a[i]), i);
+                else System.out.printf(" + %fx^%d", Math.abs(a[i]), i);
+            }
         }
-        System.out.printf("f(%f) = %f\n", X, ans);
+        System.out.printf("f(%f) = %f\n\n", X, ans);
         // Simpan jawaban dalam file
         System.out.printf("Apakah jawaban ingin disimpan dalam file?\n1. Ya\n2. Tidak\n");
         choice = sc.nextInt();
@@ -105,8 +121,22 @@ public class Interpolate {
                 FileWriter file = new FileWriter("../test/"+fileName);
                 file.write("Luaran untuk interpolasi adalah f(x) = ");
                 for(int i= n-1;i >= 0; i--){
-                    if(i == 0) file.write(Double.toString(a[i])+",\n");
-                    else file.write(Double.toString(a[i])+"x^"+Integer.toString(i)+" + ");
+                    if(i == n-1){
+                        if(a[i] < 0) file.write("-"+Double.toString(Math.abs(a[i]))+"x^"+Integer.toString(i));
+                        else file.write(Double.toString(Math.abs(a[i]))+"x^"+Integer.toString(i));
+                    }
+                    else if(i == 0){
+                        if(a[i] < 0) file.write(" - "+Double.toString(Math.abs(a[i]))+",\n");
+                        else file.write(" + "+Double.toString(Math.abs(a[i]))+",\n");
+                    }
+                    else if(i == 1){
+                        if(a[i] < 0) file.write(" - "+Double.toString(Math.abs(a[i]))+"x");
+                        else file.write(" + "+Double.toString(Math.abs(a[i]))+"x");
+                    }
+                    else{
+                        if(a[i] < 0) file.write(" - "+Double.toString(Math.abs(a[i]))+"x^"+Integer.toString(i));
+                        else file.write(" + "+Double.toString(Math.abs(a[i]))+"x^"+Integer.toString(i));
+                    }
                 }
                 file.write("f("+Double.toString(X)+") = "+Double.toString(ans)+"\n");
                 file.close();
