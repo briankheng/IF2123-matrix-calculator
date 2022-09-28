64ad82b6ac9f4b2ddc8c721506921a7bb48f7ee7
frankiehuangg <frankie.huangg@gmail.com>
2022-09-28 20:17:25 +0700

    fix: input from file; new: option to save output to file

--- src/Bicubic.java
+++ src/Bicubic.java
@@ -9,19 +9,55 @@ import java.util.Scanner;
 
 public class Bicubic
 {
-	public static void main(String[] args)
-	{
-		bicubic();
-	}
-	public static double bicubic()
+	public static void SolveBicubic()
 	// Input matrix 4x4, berisi nilai f(i,j) dan nilai (a,b)
 	// Output nilai f(a,b)
 	{
-		// invers matrix untuk -1<=x<=2 dan -1<=y<=2; range titik di antara (0,0) dan (1,1)
-		
-		// double[] matrix = {153, 125, 98, 21, 59, 161, 101, 51, 210, 72, 42, 0, 96, 81, 12, 16};
+		// Read file dari input user
+		Scanner sc = new Scanner(System.in);
+		BufferedReader scFile = new BufferedReader(new InputStreamReader(System.in));
 
-		// double a = 0.5, b = 0.5;
+		double[] input = new double[16];
+		double a = 0, b = 0;
+		int n = 0;
+
+		Boolean found = false;
+		while (!found)
+		{
+			found = true;
+			String fileName = "";
+			System.out.printf("Masukkan nama file: ");
+			try {
+				fileName = scFile.readLine();
+			}
+			catch (IOException err) {
+				err.printStackTrace();
+			}
+			try {
+                Scanner file = new Scanner(new File("../test/"+fileName));
+				while (file.hasNextDouble())
+				{
+					// System.out.println(file.nextDouble());
+					if (n == 4)
+					{
+						a = file.nextDouble();
+						b = file.nextDouble();
+					}
+					else
+					{
+						input[0+n] = file.nextDouble();
+						input[4+n] = file.nextDouble();
+						input[8+n] = file.nextDouble();
+						input[12+n] = file.nextDouble();
+					}
+					n++;
+				}
+			}
+			catch (FileNotFoundException err) {
+				err.printStackTrace();
+				found = false;
+			}
+		}
 
 		double[][] invers = {{0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 							 {0, 0, 0, 0, -1.0/3, -1.0/2, 1, -1.0/6, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -46,7 +82,7 @@ public class Bicubic
 			double sum = 0;
 			for (int j = 0; j < 16; j++)
 			{
-				sum += matrix[j] * invers[i][j];
+				sum += input[j] * invers[i][j];
 			}
 			constants.setElmt(i,0,sum);
 		}
@@ -74,6 +110,32 @@ public class Bicubic
 			}
 		}
 
-		return value;
+		System.out.println("f(" + a + "," + b + ") = " + value);
+
+		// Simpan jawaban dalam file
+        System.out.printf("Apakah jawaban ingin disimpan dalam file?\n1. Ya\n2. Tidak\n");
+        int choice = sc.nextInt();
+        while(choice != 1 && choice != 2){
+            System.out.printf("Masukan tidak valid! Silakan ulangi...\n");
+            choice = sc.nextInt();
+        }
+        if(choice == 1){
+            String fileName = "";
+            System.out.printf("Masukkan nama file: ");
+            try{
+                fileName = scFile.readLine();
+            }
+            catch(IOException err){
+                err.printStackTrace();
+            }
+            try{
+                FileWriter file = new FileWriter("../test/"+fileName);
+                file.write("f(" + Double.toString(a) + "," + Double.toString(b) + ") = " + Double.toString(value) + "\n");
+                file.close();
+            }
+            catch(IOException err){
+                err.printStackTrace();
+            }
+        }
 	}	
 }
